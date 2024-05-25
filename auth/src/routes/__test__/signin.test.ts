ccb9491c5f0b1733d3cbc93ef2ef89a29b65441c
Shahriar Tanvir <shhrrtnvr@gmail.com>
2024-05-25 21:38:07 +0600

    sign in test

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -30,3 +30,23 @@ it('fails when an incorrect password is supplied', async () => {
     .expect(400);
 });
 
+it('responds with a cookie when given valid credentials', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
+
