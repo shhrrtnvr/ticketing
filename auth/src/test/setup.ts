90aa748498ef1e74719266616fc5da904836cb55
“forhad.hossain” <forhad.hossain@tuum.com>
2024-05-25 11:15:11 +0300

    Fix with local in memory db setup

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,15 +1,20 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 
+let mongoServer: MongoMemoryServer;
+
 beforeAll(async () => {
-  process.env.JWT_KEY = 'secret';
-  const mongoUri = 'mongodb://0.0.0.0:27017/test-auth';
-  try {
-    await mongoose.connect(mongoUri, {});
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
-  }
-}); 
+  process.env.JWT_KEY = 'your_jwt_secret';
+  process.env.MONGO_URI = 'your_mongo_uri';
+  mongoServer = new MongoMemoryServer();
+  await mongoServer.start();
+  const mongoUri = await mongoServer.getUri();
+  mongoose.set('debug', true);
+  await mongoose.connect(mongoUri, { })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.error("MongoDB connection error:", err));;
+});
 
 beforeEach(async () => {
   const collections = await mongoose.connection.db.collections();
