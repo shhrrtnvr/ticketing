50bbab07539e3da88af4971c51aab01c68bc8455
Shahriar Tanvir <shhrrtnvr@gmail.com>
2024-05-26 17:14:54 +0600

    Added signup

---
+++ client/pages/_app.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+)
+
+export default App
---
+++ client/pages/auth/signup.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Handle form submission
+    await axios
+      .post('/api/users/signup', {
+        email,
+        password,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setErrors(error.response.data.errors);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1 className="heading">Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4 className="alert-heading">Error</h4>
+          <ul>
+            {errors.map((error) => (
+              <li key={error.message}>{error.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
+
+export default Signup;
