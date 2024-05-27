f98f661b90dc51c32fd3e7babd1836af303eb49e
Shahriar Tanvir <shhrrtnvr@gmail.com>
2024-05-27 15:13:40 +0600

    Added api call to services from SSR app

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,19 @@
-const Index = () => {
-  return <div>Hello World</div>;
+import { buildClient } from '../api/build-client';
+import Router from 'next/router';
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <div>Hi {currentUser.email}</div>
+  ) : (
+    <div>Not signed in, Please sign in</div>
+  );
 };
 
-export default Index;
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const path = '/api/users/currentuser';
+  const { data } = await client.get(path);
+  return data;
+};
+
+export default LandingPage;
