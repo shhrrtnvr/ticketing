9647107445a02255c2a2a1690be79b09cfb28e0c
Shahriar Tanvir <shhrrtnvr@gmail.com>
2024-06-01 00:20:34 +0600

    common library setup

---
+++ common/src/index.ts
@@ -0,0 +1,15 @@
+"use strict";
+interface Color {
+    r: number;
+    g: number;
+    b: number;
+}
+
+const color: Color = {
+    r: 255,
+    g: 0,
+    b: 0,
+};
+
+console.log(color);
+export default color;
