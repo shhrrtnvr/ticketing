462a67ec10f5c40702cfbdd5a7db62802927f23b
Shahriar Tanvir <shhrrtnvr@gmail.com>
2024-06-01 17:32:09 +0600

    Added validation tests and Ticket creation tests with model and controller

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { app } from '../../app'
+import { Ticket } from '../../models/ticket'
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app)
@@ -26,11 +27,54 @@ it('returns a status other than 401 if the user is signed in', async () => {
 })
 
 it('returns an error if an invalid title is provided', async () => {
+  const response =await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    title: '',
+    price: 10 
   }
-)
+  )
+
+  expect(response.status).toEqual(400)
+})
 
 it('returns an error if an invalid price is provided', async () => {
-  }
-)
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    title: 'asdf',
+    price: -10 
+  })
+  .expect(400)
 
-it('creates a ticket with valid inputs', async () => {})
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    title: 'asdf'
+  })
+  .expect(400)
+})
+
+it('creates a ticket with valid inputs', async () => {
+  let tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(0)
+
+  const title = 'asdf'
+  const price = 20
+
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    title, price
+  })
+  .expect(201)
+
+  tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(1)
+  expect(tickets[0].price).toEqual(price)
+  expect(tickets[0].title).toEqual(title)
+})
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,38 @@
 import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 import { requireAuth } from '@shhrrtnvrtickets/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, async (req: Request, res: Response) => {
-  res.status(201).json({ message: 'Ticket created successfully' });
+router.post('/api/tickets', 
+requireAuth,
+[
+  body('title')
+  .not()
+  .isEmpty()
+  .withMessage('Title is required'),
+  body('price')
+  .isFloat({ gt: 0 })
+  .withMessage('Price must be greater than 0'),
+],
+async (req: Request, res: Response) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const { title, price } = req.body
+
+  const ticket = Ticket.build({
+    title,
+    price,
+    userId: req.currentUser!.id
+  })
+  await ticket.save()
+
+  res.status(201).json({ message: 'Ticket created successfully' })
 })
 
 export { router as createTicketRouter}
