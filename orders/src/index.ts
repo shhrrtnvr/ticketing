e322955bc4ce6a7b09bda3e67d7ee943b57d4337
Shahriar Tanvir <shhrrtnvr@gmail.com>
2024-06-17 15:00:49 +0600

    Added expiration listener

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -5,6 +5,7 @@ import { natsWrapper } from './nats-wrapper';
 
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
+import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -48,6 +49,7 @@ const start = async () => {
 
   new TicketCreatedListener(natsWrapper.client).listen();
   new TicketUpdatedListener(natsWrapper.client).listen();
+  new ExpirationCompleteListener(natsWrapper.client).listen();
 
   app.listen(3000, () => {
     console.log('Server is running on port 3000');
