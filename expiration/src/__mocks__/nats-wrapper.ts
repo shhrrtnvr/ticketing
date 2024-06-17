a2ae2a124c2f94a5560c1c1c06402a08fae27752
Shahriar Tanvir <shhrrtnvr@gmail.com>
2024-06-17 12:27:07 +0600

    Initiated expiration service

---
+++ expiration/src/__mocks__/nats-wrapper.ts
@@ -0,0 +1,14 @@
+export const natsWrapper = {
+  client: {
+    publish: jest.fn().mockImplementation(
+      (subject: string, data: string, callback: () => void) => {
+      callback();
+    }
+  ),
+    connect: jest.fn().mockImplementation(
+        (url: string, callback: () => void) => {
+        callback();
+      }
+    ),
+  }
+};
